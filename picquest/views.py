fd67479ecd0f1ef12b61061323f6dc2b84fb137a
Dante-Cadiz <dantecadiz98@mail.com>
2022-12-18 12:31:41 +0000

    Add basic list view structure for posts.html page

--- picquest/views.py
+++ picquest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic, View
+from .models import Post
 
 # Create your views here.
 
@@ -8,10 +9,21 @@ class HomeView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
 
+
 class PostList(generic.ListView):
-    template_name = ""
+    template_name = "posts.html"
+    model = Post #temporary until filter created for today's posts only
     paginate_by = 6
 
+#class YourPosts(View):
+    #def get(self, request, *args, **kwargs):
+        #your_posts = Post.objects.filter(
+         #       author=self.request.user.id).order_by('created_on')
+        #return render(
+            #request, "your_posts.html",
+            #{
+               # "your_posts": your_posts,
+           # },)
 #class AddPost(View):
     #def post(self, request, slug, *args, **kwargs):
 
