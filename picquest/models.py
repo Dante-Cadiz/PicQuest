a2aeb74d8df0f95be28b925165e8f41e2f056f5a
Dante-Cadiz <dantecadiz98@mail.com>
2022-12-18 19:24:13 +0000

    Add ordering to post model

--- picquest/models.py
+++ picquest/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(
         User, related_name='like', blank=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
         return self.title
